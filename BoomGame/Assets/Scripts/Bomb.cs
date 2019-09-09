38fb2ff950c0bc690fb2cf5608c832b761c18e17
larikiv <lari@ratcrewstudios.com>
2019-09-10 01:56:27 +0300

    BoomGame first prototype

---
+++ BoomGame/Assets/Scripts/Bomb.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    //Delay in seconds before exploding
+    public float delay = 1.0f;
+
+    public float destroyRadius = 1.0f;
+    public float radius = 1.0f;
+    public float power = 100.0f;
+    public bool showGizmo = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Explode());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        if (showGizmo)
+        {
+            Gizmos.color = Color.yellow;
+
+            Gizmos.DrawSphere(this.transform.position, radius);
+
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawSphere(this.transform.position, destroyRadius);
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(delay);
+        Vector3 explosionPos = transform.position;
+
+        Collider2D[] destroyColliders = Physics2D.OverlapCircleAll(explosionPos, destroyRadius);
+        foreach (Collider2D hit in destroyColliders)
+        {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+
+
+        foreach (Collider2D hit in colliders)
+        {
+            //SHattering object shatter in to multiple parts/bricks
+            if (hit.gameObject.tag.Contains("ShatteringObject"))
+            {
+                hit.gameObject.GetComponent<BrickLogic>().Shatter(explosionPos, power);
+            }
+            else
+            {
+                Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+                if (rb != null)
+                {
+                    // Calculating the direction from the bomb placement to the overlapping 
+                    Vector2 heading = hit.transform.position - explosionPos;
+                    float distance = heading.magnitude;
+                    Vector2 direction = heading / distance;
+
+                    //Calculate force from the direction multiplied by the power. Force weaker by distance
+                    Vector2 force = direction * (power / distance);
+
+                    rb.AddForce(force, ForceMode2D.Impulse);
+
+                    //rb.AddForce.AddExplosionForce(power, explosionPos, radius, 3.0F, ForceMode.Impulse);
+                }
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ BoomGame/Assets/Scripts/BrickLogic.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickLogic : MonoBehaviour
+{
+    public Vector2[] pieceSpawnLocations;
+    public GameObject[] pieceObjects;
+
+
+    void OnDrawGizmos()
+    {
+        if (pieceSpawnLocations.Length > 0)
+        {
+            foreach (var pieceLocation in pieceSpawnLocations)
+            {
+                Gizmos.color = Color.blue;
+
+                Gizmos.DrawSphere(this.transform.position + new Vector3(pieceLocation.x, pieceLocation.y, 0.0f), 0.2f);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Shatters the brick into smaller pieces
+    /// Spawn new game object instances and sends them flying away from the force
+    /// </summary>
+    /// <param name="explosionPos"></param>
+    /// <param name="power"></param>
+    public void Shatter(Vector3 explosionPos, float power)
+    {
+        List<GameObject> newObjects = new List<GameObject>();
+        for (int e = 0; e < pieceObjects.Length; e++)
+        {
+            var newObject = Instantiate(pieceObjects[e]);
+
+            //Set the position showed by the gizmos
+            if (pieceSpawnLocations.Length >= e)
+            {
+                newObject.transform.position = new Vector3(this.transform.position.x + pieceSpawnLocations[e].x, this.transform.position.y + pieceSpawnLocations[e].y, transform.position.z);
+            }
+            newObjects.Add(newObject);
+
+        }
+
+
+        Destroy(this.gameObject);
+
+
+        foreach (var newObject in newObjects)
+        {
+            Rigidbody2D rb = newObject.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                // Calculating the direction from the bomb placement to the overlapping 
+                Vector2 heading = newObject.transform.position - explosionPos;
+                float distance = heading.magnitude;
+                Vector2 direction = heading / distance;
+
+                //Calculate force from the direction multiplied by the power. Force weaker by distance
+                Vector2 force = direction * (power / distance);
+                rb.AddForce(force, ForceMode2D.Impulse);
+
+                //rb.AddForce.AddExplosionForce(power, explosionPos, radius, 3.0F, ForceMode.Impulse);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
