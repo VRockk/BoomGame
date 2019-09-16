3af0ca55feee4c8312b837bef1c7ebc7a1fe0ecd
Li Lan <c7lali00@students.oamk.fi>
2019-09-16 10:38:20 +0300

    MainMenu

---
+++ BoomGame/Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLogic : MonoBehaviour
+{
+    public void GotoLiScene()
+    {
+        SceneManager.LoadScene("Li Scene");
+    }
+
+    public void GotoNewScene()
+    {
+        SceneManager.LoadScene("New Scene");
+    }
+}
