ba4fd0b7cdab628a78de5840defca08ddb24a92e
Li Lan <c7lali00@students.oamk.fi>
2019-09-16 13:28:45 +0300

    Add volume setting in MainMenu

---
+++ BoomGame/Assets/SetMenuVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMenuVolume : MonoBehaviour
+{
+    private AudioSource audioSrc;
+    private float musicVolume = 1f;
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
