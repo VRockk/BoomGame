0c398b575e5235fd33a532cce41f1c0e10b025d5
Lari <larikiv@gmail.com>
2019-09-19 13:37:08 +0300

    Menus icons

---
+++ BoomGame/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Transform camTransform;
+
+    // Desired duration of the shake effect
+    private float shakeDuration = 0f;
+
+    // A measure of magnitude for the shake. Tweak based on your preference
+    private float shakeMagnitude = 0.7f;
+
+    // A measure of how quickly the shake effect should evaporate
+    private float dampingSpeed = 1.0f;
+
+    // The initial position of the GameObject
+    private Vector3 initialPosition;
+
+    void Awake()
+    {
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+
+    public void Shake(float duration, float magnitude, float damping)
+    {
+        camTransform = Camera.main.transform;
+        shakeDuration = duration;
+        shakeMagnitude = magnitude;
+        dampingSpeed = damping;
+    }
+}
