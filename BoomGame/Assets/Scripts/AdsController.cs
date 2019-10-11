f727ea2dc64281a716cb4296e45076226bab769c
Li Lan <c7lali00@students.oamk.fi>
2019-10-11 16:01:43 +0300

    ad plugin

---
+++ BoomGame/Assets/Scripts/AdsController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+// Call this way
+// AdsController.adsInstance.ShowVideoOrInterstitialAds();
+public class AdsController : MonoBehaviour
+{
+    public static AdsController adsInstance;
+    
+    
+    private string googlePlayStoreId = "3318672";
+    private string appleAppStoreId = "3318673";
+
+    private string video_ad = "video";
+    private string rewarded_video_ad = "rewardedVideo";
+    private string banner_ad = "bannerAds";
+
+    private void Awake()
+    {
+        if(adsInstance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            adsInstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //Check the platform
+        string gameId = googlePlayStoreId;
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            gameId = appleAppStoreId;
+        }
+        Monetization.Initialize(gameId, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*    
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Monetization.IsReady(video_ad)){
+                ShowAdPlacementContent ad = null;
+                ad = Monetization.GetPlacementContent(video_ad) as ShowAdPlacementContent;
+
+                if (ad != null)
+                {
+                    ad.Show();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (Monetization.IsReady(rewarded_video_ad))
+            {
+                Debug.Log("test");
+                ShowAdPlacementContent ad = null;
+                ad = Monetization.GetPlacementContent(rewarded_video_ad) as ShowAdPlacementContent;
+
+                if (ad != null)
+                {
+                    ad.Show();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (Monetization.IsReady(banner_ad))
+            {
+                Debug.Log("test");
+                ShowAdPlacementContent ad = null;
+                ad = Monetization.GetPlacementContent(banner_ad) as ShowAdPlacementContent;
+
+                if (ad != null)
+                {
+                    ad.Show();
+                }
+            }
+        }*/
+    }
+    public void ShowVideoOrInterstitialAds()
+    {
+        //Play videoAds
+        if (Monetization.IsReady(video_ad))
+        {
+            ShowAdPlacementContent ad = null;
+            ad = Monetization.GetPlacementContent(video_ad) as ShowAdPlacementContent;
+
+            if (ad != null)
+            {
+                ad.Show();
+            }
+        }
+    }
+}
