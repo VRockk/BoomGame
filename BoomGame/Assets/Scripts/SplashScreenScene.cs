7afec2514856b1f8ec2370669a083c742652cacf
Li Lan <c7lali00@students.oamk.fi>
2019-10-22 14:57:29 +0300

    Splash Screen was done

---
+++ BoomGame/Assets/Scripts/SplashScreenScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScene : MonoBehaviour
+{
+    public float WaitTime = 7f;
+   
+    void Start()
+    {
+        StartCoroutine(WaitForSplash());
+        
+    }
+
+    IEnumerator WaitForSplash() {
+        yield return new WaitForSeconds(WaitTime);
+        SceneManager.LoadScene("MainMenuScene");
+    }
+  
+}
