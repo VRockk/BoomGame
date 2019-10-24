3fa020849eb1b13bf9ebaadeb93a9338e58d56e8
Joel Panula <n7pajo02@students.oamk.fi>
2019-10-24 13:34:40 +0300

    Saving levels

--- BoomGame/Assets/Scripts/CampaignMap.cs
+++ BoomGame/Assets/Scripts/CampaignMap.cs
@@ -2,24 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CampaignMap : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameController gameController;
+    public GameMaster gameMaster;
+    public Button[] levelButtons;
+    
+
     void Start()
     {
-        
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+                levelButtons[i].interactable = false;
+        }
+
+        //PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + gameController.salvageValue);
+        //if ()
+        //{
+
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 
     public void OpenLevel(string name)
     {
         //TODO show "loading" screen.
         SceneManager.LoadScene(name);
+        
     }
 }
