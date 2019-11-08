7f554c2cb40ccf0a574531219e012aaba71eda38
Lari <larikiv@gmail.com>
2019-11-08 12:39:31 +0200

    Fixed referencing scenes

--- BoomGame/Assets/Scripts/CampaignMap/Level.cs
+++ BoomGame/Assets/Scripts/CampaignMap/Level.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    public SceneAsset scene;
+    public Object scene;
 
     [HideInInspector]
     public int pentagrams = 0;
@@ -14,29 +13,28 @@ public class Level : MonoBehaviour
     [HideInInspector]
     public int score = 0;
 
+
     //public string[] maps;
 
     // Start is called before the first frame update
     void Start()
     {
     }
+    void OnValidate()
+    {
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void GetSavedValues()
     {
-        if (scene == null)
-            Debug.LogWarning("No scene file set for level object: " + gameObject.name);
-        else
-        {
-            var name = scene.name;
-            pentagrams = PlayerPrefs.GetInt(name + "Pentagrams", 0);
-            savedBombs = PlayerPrefs.GetInt(name + "SavedBombs", 0);
-            score = PlayerPrefs.GetInt(name + "Score", 0);
-        }
+        print(scene.name);
+        pentagrams = PlayerPrefs.GetInt(scene.name + "Pentagrams", 0);
+        savedBombs = PlayerPrefs.GetInt(scene.name + "SavedBombs", 0);
+        score = PlayerPrefs.GetInt(scene.name + "Score", 0);
     }
 }
